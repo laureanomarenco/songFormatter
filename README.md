93cc4307197f9ccf2f6da535b7990a55cfaa2e05
Laureano Marenco <laureanomarenco@gmail.com>
2023-02-03 10:39:25 -0300

    promesas

--- README.md
+++
@@ -1,2 +0,0 @@
-californication
-losing my religion
--- js/main.js
+++ js/main.js
@@ -1,45 +1,49 @@
-// Home 
-fetch('http://localhost:3000/cancion')
-    .then(res => res.json())
-    .then(cancion => {
-        const table = document.querySelector('#body-table')
+// ** HOME **
+const fetchAll = async () => {
+    // ** Renderiza tabla **
+    const aux = await fetch('http://localhost:3000/cancion')
+    const cancion = await aux.json()
 
-        for (let i = 0; i < cancion.length; i++) {
-            const tr = document.createElement('tr')
+    const aux2 = await fetch('http://localhost:3000/autor')
+    const autor = await aux2.json()
+    const table = document.querySelector('#body-table')
+
+    for (let i = 0; i < cancion.length; i++) {
+        const tr = document.createElement('tr')
+        const td = document.createElement('td')
+        const a = document.createElement('a')
+
+        a.innerText = cancion[i].titulo
+        td.appendChild(a)
+        tr.appendChild(td)
+
+        for (let j = 0; j < autor.length; j++) {
             const td = document.createElement('td')
             const a = document.createElement('a')
-
-            a.innerText = cancion[i].titulo
+            if (autor[j].idAutor === cancion[i].idAutor) {
+                a.innerText = autor[j].nombre
+            } else {
+                a.innerText = "Desconocido"
+            }
             td.appendChild(a)
             tr.appendChild(td)
+            const td2 = document.createElement('td')
+            const p = document.createElement('p')
 
-            fetch('http://localhost:3000/autor')
-                .then(res => res.json())
-                .then(autor => {
-                    for (let i = 0; i < autor.length; i++) {
-                        const td = document.createElement('td')
-                        const a = document.createElement('a')
+            p.innerText = cancion[i].anio
+            td2.appendChild(p)
+            tr.appendChild(td2)
 
-                        a.innerText = autor[i].nombre
-                        td.appendChild(a)
-                        tr.appendChild(td)
+            const td3 = document.createElement('td')
+            td3.classList.add("add")
+            const a2 = document.createElement('a')
 
-                        const td2 = document.createElement('td')
-                        const p = document.createElement('p')
-            
-                        p.innerText = cancion[i].anio
-                        td2.appendChild(p)
-                        tr.appendChild(td2)
-
-                        const td3 = document.createElement('td')
-                        td3.classList.add("add")
-                        const a2 = document.createElement('a')
-            
-                        a2.innerText = "+"
-                        td3.appendChild(a2)
-                        tr.appendChild(td3)
-                        table.appendChild(tr)
-                    }
-                })
+            a2.innerText = "+"
+            td3.appendChild(a2)
+            tr.appendChild(td3)
+            table.appendChild(tr)
         }
-    })
+    }
+}
+
+fetchAll();
